62bc338695dfa1756dabf055b1b7fb72a59d8f16
Yuri9828835868 <135593196+Yuri9828835868@users.noreply.github.com>
2025-02-25 21:54:26 +0545

    Not working

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    "Body Mass Index (BMI) is a measurement of a person's weight \n"
-    "with respect to their height. It is an indicator rather than \n"
-    "a direct measurement of total body fat. BMI helps assess \n"
-    "whether an individual is underweight, normal, overweight, \n"
-    "or obese based on standardized categories.
